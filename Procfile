2770350060d9b5bb50a3e74ecbbf7f12061d7a6a
Hector Gonzalez <gonvalhector@gmail.com>
2020-07-09 11:56:05 -0600

    added Heroku settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn r8books.wsgi --log-file -
--- r8books/settings.py
+++ r8books/settings.py
@@ -14,6 +14,7 @@ import os
 import os.path
 from os import path
 import json
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,4 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+django_heroku.settings(locals())
