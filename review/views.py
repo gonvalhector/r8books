8666e80719150c01f3aff95dff36625e5c08feb9
Hector Gonzalez <gonvalhector@gmail.com>
2020-07-21 16:51:29 -0600

    added urls and basic views, finished layout.html

--- review/views.py
+++ review/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    return render(request, 'review/index.html')
+
+def register(request):
+    return HttpResponse('TODO /register')
+
+def login_view(request):
+    return HttpResponse('TODO /login')
+
+def logout_view(request):
+    return HttpResponse('TODO /logout')
