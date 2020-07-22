1f305196073d34cb02d5592c9fd253c946607b85
Hector Gonzalez <gonvalhector@gmail.com>
2020-07-22 12:19:05 -0600

    progress on index, register, login and logout views

--- review/views.py
+++ review/views.py
@@ -1,18 +1,38 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
     if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse("login"))
+        return HttpResponseRedirect(reverse('login'))
     return render(request, 'review/index.html')
 
 def register(request):
-    return HttpResponse('TODO /register')
+    '''Display user registration form and register a new user.'''
+    # When the user is redirected or they follow a link
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            message = 'You are logged in. Log out to create an account.'
+        else:
+            message = None
+        return render(request, 'review/register.html', message)
+    # When the user submits a form (POST)
+    else:
+        # If the user is currently logged in
+        if request.user.is_authenticated:
+            return render(request, 'review/register.html', {'message': 'Please log out before registering a new account.'})
+
 
 def login_view(request):
-    return HttpResponse('TODO /login')
+    '''Display the login page and log user in.'''
+    # When the user is redirected or they follow a link
+    if request.method == 'GET':
+        return render(request, 'review/login.html')
 
 def logout_view(request):
-    return HttpResponse('TODO /logout')
+    '''Log user out.'''
+    # When the user is redirected or they follow a link
+    if request.method == 'GET':
+        return HttpResponse('TODO /logout')
